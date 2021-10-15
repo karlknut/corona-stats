c8adb99713e687f44d17baf575cd764315d45dbf
Kaspar Martin Suursalu <kasparsu@gmail.com>
2021-10-15 14:58:50 +0300

    add simple pagination

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,32 @@
 <template>
-<v-row>
-  <v-col :cols="3" v-for="(result, index) in results" :key="'result-' + result.id">
-    <v-card elevation="2">
-      <v-card-title>{{result.name}}</v-card-title>
-      <v-card-text>
-        <ul>
-          <li><b>Status:</b>{{result.status}}</li>
-          <li><b>Gender:</b>{{result.gender}}</li>
-          <li><b>Species:</b>{{result.species}}</li>
-        </ul>
-      </v-card-text>
-    </v-card>
-  </v-col>
-</v-row>
+  <div>
+    <v-btn class="my-3" @click="prev" :disabled="!pagination.prev">Prev</v-btn>
+    <v-btn class="float-right my-3" @click="next" :disabled="!pagination.next">Next</v-btn>
+    <v-row>
+      <v-col :cols="3" v-for="result in results" :key="'result-' + result.id">
+        <v-card elevation="2">
+          <v-img :src="result.image"></v-img>
+          <v-card-title>{{result.name}}</v-card-title>
+          <v-card-text>
+            <ul>
+              <li><b>Status:</b>{{result.status}}</li>
+              <li><b>Gender:</b>{{result.gender}}</li>
+              <li><b>Species:</b>{{result.species}}</li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
 export default {
   created(){
     this.$axios.get('https://rickandmortyapi.com/api/character').then(response => {
-      console.log(response);
       this.results = response.data.results;
       this.pagination = response.data.info;
-    })
+    });
   },
   data(){
     return {
@@ -33,6 +37,20 @@ export default {
         pages: 0,
         prev: null,
       }
+    }
+  },
+  methods: {
+    next(){
+      this.$axios.get(this.pagination.next).then(response => {
+        this.results = response.data.results;
+        this.pagination = response.data.info;
+      });
+    },
+    prev(){
+      this.$axios.get(this.pagination.prev).then(response => {
+        this.results = response.data.results;
+        this.pagination = response.data.info;
+      });
     }
   }
 }
