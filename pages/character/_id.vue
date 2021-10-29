2409da7dea63d8ce76a4bdce2133798b62c53dce
Kaspar Martin Suursalu <kasparsu@gmail.com>
2021-10-29 14:36:43 +0300

    add single character page

--- pages/character/_id.vue
+++ pages/character/_id.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>{{$route.params.id}}</h1>
+  <v-card elevation="2" v-if="character">
+    <v-img :src="character.image"></v-img>
+    <v-card-title>{{character.name}}</v-card-title>
+    <v-card-text>
+      <ul>
+        <li><b>Status:</b>{{character.status}}</li>
+        <li><b>Gender:</b>{{character.gender}}</li>
+        <li><b>Species:</b>{{character.species}}</li>
+      </ul>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
 export default {
-
+  created(){
+    this.$axios.get('https://rickandmortyapi.com/api/character/'+ this.$route.params.id).then(response => {
+      this.character = response.data;
+    }); 
+  },
+  data(){
+    return {
+      character: null
+    }
+  }
 }
 </script>
 
